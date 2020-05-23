dc062681526e3ed030b8702d0bcee6ad29ae2884
Marcus J <marcus.seven@live.se>
2020-05-23 13:44:17 +0200

    conenction, form1 ändringar

--- KleenBio/Form1.Designer.cs
+++ KleenBio/Form1.Designer.cs
@@ -29,7 +29,38 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 61);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(106, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Lista kunder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(176, 61);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(456, 160);
+            this.listBox1.TabIndex = 1;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 133);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
             // 
             // Form1
             // 
@@ -37,13 +68,22 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- KleenBio/Form1.cs
+++ KleenBio/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +13,48 @@ namespace KleenBio
 {
     public partial class Form1 : Form
     {
+        string ConnString = "SERVER=localhost;DATABASE=kinoprogramme;UID=olas;PASSWORD=ola123;";
+
+        List<kunder> AllaKunder;
+        kunder aktuellKund;
+
         public Form1()
         {
             InitializeComponent();
+            AllaKunder = new List<kunder>();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MySqlConnection connection = new MySqlConnection(ConnString);
+            connection.Open();
+
+            MySqlCommand cmd = kunder.getAll();
+            MySqlDataAdapter datAdapt = new MySqlDataAdapter();
+            datAdapt.SelectCommand = cmd;
+            cmd.Connection = connection;
+            DataTable dt = new DataTable();
+            datAdapt.Fill(dt);
+
+            foreach(DataRow row in dt.Rows)
+            {
+                AllaKunder.Add(new kunder(row));
+            }
+            listBox1.DataSource = AllaKunder;
+            connection.Close();
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            aktuellKund = (kunder)listBox1.SelectedItem;
+            
+            
         }
     }
 }
